9b8a8d4a8a6a9be43cc82b952bc95f2b89ae4997
greengoblin8 <134504798+greengoblin8@users.noreply.github.com>
2025-03-24 02:01:48 +0700

    implementasi design by contract

---
+++ tpmod6_103022300015/tpmod6_103022300015/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace tpmod6_103022300015
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SayaTubeVideo video = new SayaTubeVideo("Tutorial design by contract - Gamaliel Pradana");
+                video.printVideoDetail();
+                Console.WriteLine();
+                for (int i = 0; i < 1000; i++)
+                {
+                    video.increasePlayCount(10000000);
+                    video.printVideoDetail();
+                    Console.WriteLine();
+
+                    if (video.getPlaycount() == int.MaxValue)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error!" + e.Message);
+            }
+
+        }
+    }
+}
---
+++ tpmod6_103022300015/tpmod6_103022300015/SayaTubeVideo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod6_103022300015
+{
+     class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+        Random random = new Random();
+
+        public SayaTubeVideo(String title)
+        {
+            Debug.Assert(title != null, "Judul tidak boleh kosong");
+            Debug.Assert(title.Length <= 100, "Judul tidak lebih dari 100 kata ");
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playcount = 0;
+            
+        }
+
+        public void increasePlayCount(int views) {
+            Debug.Assert(views <= 10000000, "Jumlah views diantara 1 hingga 10000000");
+
+            try
+            {
+                checked
+                {
+                    if (this.playcount > int.MaxValue - views)
+                    {
+
+                        throw new OverflowException();
+                    }
+                    this.playcount += views;
+                }
+            }
+            catch (OverflowException) 
+            {
+                Console.WriteLine("Terjadi Kesalahan karena sudah mencapai batas maksimum views");
+                this.playcount -= views;
+            }
+        }
+
+        public void printVideoDetail()
+        {
+            Console.WriteLine("ID video: " + this.id);
+            Console.WriteLine("Judul video: " + this.title);
+            Console.WriteLine("Jumlah penonton: " + this.playcount);
+        }
+        public int getPlaycount()
+        {
+            return this.playcount;
+        }
+    }
+}
